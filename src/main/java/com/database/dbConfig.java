d3be0b979dc4ab01764d88e7b10b74ea603e574f
dhivyanand <dhivyananthan98@gmail.com>
2019-01-07 20:00:46 +0530

    Completed Database Connection class.

--- src/main/java/com/database/dbConfig.java
+++ src/main/java/com/database/dbConfig.java
@@ -3,5 +3,6 @@ public class dbConfig{
     public static String username;
     public static String password;
     public static String host;
+    public static String database;
 
 }
---
+++ src/main/java/com/database/dbConnection.java
@@ -0,0 +1,31 @@
+import java.sql.Connection;
+import java.sql.Statement;
+
+public class dbConnect{
+
+    protected Statement getConnection(){
+
+        String username = dbConfig.username;
+        String password = dbConfig.password;
+        String database = dbConfig.database;
+        String host = dbConfig.host;
+
+        String url = "jdbc:mysql://localhost/"+database;
+
+        Connection connection;
+        Statement statement;
+
+        try{
+
+            connection = DriverManager.getConnection(host, username, password);
+            statement = connection.createStatement();
+            
+            return statement; 
+
+        }catch(Exception e){
+            return null;
+        }
+
+    }
+
+}
